56000e919122f948dc60c6012c845f6d96853e1b
Jaehoan Kim <k1mjh6561@tamu.edu>
2023-06-16 16:22:12 +0900

    add replicable codes in SPDE

--- Result_Manuscript/Result_GPI1D.R
+++ Result_Manuscript/Result_GPI1D.R
@@ -9,16 +9,14 @@ sourceCpp("1D/GPI/inv_chol.cpp")
 source("1D/GPI/functions_GPI.R")
 source("1D/GPI/functions_GPI_sampling.R")
 
-n = 200 # the number of observed data; 200, 500, 1000
-filename = paste0("Result_Manuscript/obs_n1D", n, ".RData")
-load(filename)
-
-
 target = 250
 brn.ESS = 100
 kappa = 2
 dpois5 = function(x){
    return(dpois(x, lambda = 5))
+}
+const = function(x){
+   return(1)
 }
 
 f0_1D = function(x){return(1*x^2+sin(8*x))}
@@ -27,23 +25,28 @@ alpha2 = 0.95 # coverage probability 2 (light gray area)
 Nk = c(4, 6, 8, 10, 12)
 
 M = 50
-MSE_list = vector(length = M)
-for(m in 1:M){  
-   # m th dataset among M = 50 dataset
-   X = df$X[((m-1)*n+1):(m*n)]
-   Y = df$Z[((m-1)*n+1):(m*n)]
-   result = sample.ESS.seq(X, Y, sigsq = 0.1^2, Nk = Nk, nu.in = 1, l.in = 1/kappa,
-                           N.pr = dpois5,
-                           mcmc = target, brn=0, brn.ESS = brn.ESS)
-   g.plot = tail(result$g_list, target) # choosing last `target` samples
-   grid.plot = c(0:1000)/1000
-   obs = data.frame(X, Y)
-   y.plot = glist_to_plotdf(g.plot, grid.plot, true = f0_1D, alpha1 = 0.95, alpha2 = 0.9)
-   MSE_list[m] = mean((y.plot$true - y.plot$mean)^2)
-   print(m)
+nlist = c(200, 500, 1000)
+MSE_list = matrix(nrow = M, ncol = length(nlist))
+
+for(a in 1:length(nlist)){
+   n = nlist[a] # the number of observed data; 200, 500, 1000
+   filename = paste0("Result_Manuscript/obs_n1D", n, ".RData")
+   load(filename)
+   for(m in 1:M){  
+      # m th dataset among M = 50 dataset
+      X = df$X[((m-1)*n+1):(m*n)]
+      Y = df$Z[((m-1)*n+1):(m*n)]
+      result = sample.ESS.seq(X, Y, sigsq = 0.1^2, Nk = Nk, nu.in = 1, l.in = 1/kappa,
+                              N.pr = const,
+                              mcmc = target, brn=0, brn.ESS = brn.ESS)
+      g.plot = tail(result$g_list, target) # choosing last `target` samples
+      grid.plot = c(0:1000)/1000
+      obs = data.frame(X, Y)
+      y.plot = glist_to_plotdf(g.plot, grid.plot, true = f0_1D, alpha1 = 0.95, alpha2 = 0.9)
+      MSE_list[m, a] = mean((y.plot$true - y.plot$mean)^2)
+      print(m)
+   }
 }
-
-
 
 ## Plot 3. Fitting of the samples (credible intervals)
 
@@ -57,11 +60,23 @@ ggplot(y.plot, aes(x = x)) +
    geom_ribbon(aes(ymin=low1, ymax=upp1),  alpha=0.4, show.legend=TRUE) + 
    geom_ribbon(aes(ymin=lowsup, ymax=uppsup),  alpha=0.2, show.legend=TRUE) +
    geom_point(aes(x = x, y = true), col = 'red', size = 0.5) +
-   geom_point(data = obs, aes(X, Y), size = 0.3) +
-   labs(title = paste0("CI 95% of ", target, " samples (n = ", n, ")"))+
-   geom_point(data = obs, aes(X, Y), size = 0.3)
+   # geom_point(data = obs, aes(X, Y), size = 0.3) +
+   labs(title = paste0("CI 95% of ", target, " samples (n = ", n, ")"))
 
 
 N_list = tail(result$N_list, target)
 plot(N_list)
 lines(N_list)
+
+## Plot 4. For the replicated result ##
+MSE.df = data.frame(MSE_list)
+colnames(MSE.df) = nlist
+MSE.df.plot = gather(MSE.df, key = "n", value = "MSE")
+MSE.df.plot$n <- factor(MSE.df.plot$n, levels = c(200, 500, 1000))
+   
+
+## save as Rdata
+# save(MSE.df.plot, file = "Result_Manuscript/MSE_dataframe/MSE_GPI_1D.RData")
+
+ggplot(MSE.df.plot) + 
+   geom_boxplot(aes(x = n, y = MSE)) + themegg
--- Result_Manuscript/Result_SPDE1D.R
+++ Result_Manuscript/Result_SPDE1D.R
@@ -6,42 +6,47 @@ library(Matrix)
 source("1D/SPDE/functions_SPDE_sampling.R")
 source("1D/SPDE/functions_SPDE.R")
 
-n = 200 # the number of observed data; 200, 500, 1000
-filename = paste0("Result_Manuscript/obs_n1D", n, ".RData")
-load(filename)
-m = 1 # m th dataset among M = 50 dataset
-X = df$X[((m-1)*n+1):(m*n)]
-Y = df$Z[((m-1)*n+1):(m*n)]
 target = 2500
 brn.ESS = 100
 kappa = 2
-dpois5 = function(x){
-   return(dpois(x, lambda = 5))
+
+const = function(x){
+   return(1)
 }
 
-## discretized version of 1 over exponential distribution - hich satisfy the condition for prior theoretically
-# N.pr = function(N){return (1/N^2 * 3 * exp(-3 / N))}
-target = 100
-brn = 100
-kappa = 2
-Nk = c(3, 5, 10, 15, 20)
-result = sample.exact.seq(X, Y, sigsq = 0.1^2, Nk = Nk, N.pr = dpois5,
-                          kappa.init = kappa, mcmc = target, brn=brn, seed = 1234)
+f0_1D = function(x){return(1*x^2+sin(8*x))}
+alpha1 = 0.9 # coverage probability 1 (dark gray area)
+alpha2 = 0.95 # coverage probability 2 (light gray area)
+Nk = c(4, 6, 8, 10, 12)
 
+M = 50
+nlist = c(200, 500, 1000)
+MSE_list = matrix(nrow = M, ncol = length(nlist))
+
+for(a in 1:length(nlist)){
+   n = nlist[a]
+   filename = paste0("Result_Manuscript/obs_n1D", n, ".RData")
+   load(filename)
+   for(m in 1:M){
+      # m th dataset among M = 50 dataset
+      X = df$X[((m-1)*n+1):(m*n)]
+      Y = df$Z[((m-1)*n+1):(m*n)]
+      result = sample.exact.seq(X, Y, sigsq = 0.1^2, Nk = Nk, N.pr = const,
+                                kappa.init = kappa, mcmc = target, brn=brn, seed = 1234)
+      g.plot = tail(result$g_list, target) # choosing last `target` samples
+      grid.plot = c(0:1000)/1000
+      obs = data.frame(X, Y)
+      y.plot = glist_to_plotdf(g.plot, grid.plot, true = f0_1D, alpha1 = 0.95, alpha2 = 0.9)
+      MSE_list[m, a] = mean((y.plot$true - y.plot$mean)^2)
+      print(m)
+   }
+}
 ## Plot 3. Fitting of the samples (credible intervals)
 
 ################## converting predicted values into usable form ##################
 ##################################################################################
 
-f0_1D = function(x){return(1*x^2+sin(8*x))}
-alpha1 = 0.9 # coverage probability 1 (dark gray area)
-alpha2 = 0.95 # coverage probability 2 (light gray area)
-sample.num = target # number of samples used for the credible intervals
-g.plot = tail(result$g_list, sample.num) # choosing last `sample.num` samples
-grid.plot = c(0:1000)/1000
 obs = data.frame(X, Y)
-
-y.plot = glist_to_plotdf(g.plot, grid.plot, true = f0_1D, alpha1 = 0.95, alpha2 = 0.9)
 
 ggplot(y.plot, aes(x = x)) +
    geom_line(aes(y=mean), colour="blue") + 
