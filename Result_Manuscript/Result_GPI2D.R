6c3ea6be929d30a5bbb76ce2369b7f77994a27b1
Jaehoan Kim <k1mjh6561@tamu.edu>
2023-07-12 12:12:51 +0900

    modify files to generate heatmap

--- Result_Manuscript/Result_GPI2D.R
+++ Result_Manuscript/Result_GPI2D.R
@@ -26,37 +26,20 @@ f0 = function(x, y){
 gridsize = 40
 gridmat = cbind(rep(c(0:gridsize)/gridsize, each = gridsize + 1),
                 rep(c(0:gridsize)/gridsize, gridsize+ 1))
-MSE_list = matrix(nrow = M, ncol = length(nlist))
 
 ############################################################
-
-for(a in 1:length(nlist)){
-   n = nlist[a]
-   filename = paste0("Result_Manuscript/obs_n2D", n, ".RData")
-   load(filename)
-   for(m in 1:M){
-      X = df[((m-1)*n+1):(m*n), c(1, 2)]
-      Z = df$Z[((m-1)*n+1):(m*n)]
-      result = sample.RJESS2D.seq(Z = Z, X = X, N.pr = function(x){return(1)}, Nk = Nk, sigsq = 0.1^2,
-                                  mcmc = target, brn = 0, nu.in = 1, l.in = 1/kappa, brn.ESS = brn.ESS)
-      g_list = result$g_list
-      y.plot = glist_to_plotdf_2D(g_list, gridmat, truefun = f0, alpha1 = 0.9, alpha2 = 0.95)
-      MSE_list[m, a] = mean((y.plot$truefun - y.plot$mean)^2)
-      print(m)
-   }
-}
-
-## Plot 4. For the replicated result ##
-library(tidyverse)
-MSE.df = data.frame(MSE_list)
-colnames(MSE.df) = nlist
-MSE.df.plot = gather(MSE.df, key = "n", value = "MSE")
-MSE.df.plot$n <- factor(MSE.df.plot$n, levels = nlist)
-ggplot(MSE.df.plot) + 
-   geom_boxplot(aes(x = n, y = MSE))
-## save as Rdata
-save(MSE.df.plot, file = "Result_Manuscript/MSE_dataframe/MSE_GPI_2D.RData")
-
+## specify n
+a =  1
+n = nlist[a]
+filename = paste0("Result_Manuscript/obs_n2D", n, ".RData")
+load(filename)
+## specify dataset
+m = 1
+X = df[((m-1)*n+1):(m*n), c(1, 2)]
+Z = df$Z[((m-1)*n+1):(m*n)]
+result = sample.RJESS2D.seq(Z = Z, X = X, N.pr = function(x){return(1)}, Nk = Nk, sigsq = 0.1^2,
+                            mcmc = target, brn = 0, nu.in = 1, l.in = 1/kappa, brn.ESS = brn.ESS)
+g_list = result$g_list
 ################## plot ###################
 library(ggpubr)
 
@@ -95,10 +78,11 @@ plot_upp2 <- ggplot(y.plot, aes(x1, x2)) +
 ## plot arrangement : list(1, 2, 3, 4) => 1 2 // 3 4
 final_plot = ggarrange(plotlist = list(plot_true, plot_mean, 
                                        plot_low2, plot_upp2), nrow = 2, ncol = 2)
-final_plot
-MSE = mean((y.plot$truefun - y.plot$mean)^2)
-MSE
 
+fileloc = "Result_Manuscript/heatmap/"
+pdf(file = paste0(fileloc, "heatmap_GPI_2D.pdf"))
+print(final_plot)
+dev.off()
 
 N_list = tail(result$N_list, target)
 par(mfrow = c(1, 2))
--- Result_Manuscript/Result_SPDE2D.R
+++ Result_Manuscript/Result_SPDE2D.R
@@ -27,36 +27,20 @@ nlist = c(200, 500, 1000)
 # gridmat is a (gridsize^2) by 2 matrix!
 gridmat = cbind(rep(c(0:gridsize)/gridsize, each = gridsize + 1),
                 rep(c(0:gridsize)/gridsize, gridsize+ 1))
-MSE_list = matrix(nrow = M, ncol = length(nlist))
 
 ############################################################
-for(a in 1:length(nlist)){
-   n = nlist[a]
-   filename = paste0("Result_Manuscript/obs_n2D", n, ".RData")
-   load(filename)
-   for(m in 1:M){ # m th dataset among M = 50 dataset
-      X = df[((m-1)*n+1):(m*n), c(1, 2)]
-      Z = df$Z[((m-1)*n+1):(m*n)]
-      result = sample.exact2D.seq(X, Z, sigsq = 0.1^2, # N.pr = function(x){return(1)},
-                                  N.pr = dpoi5,
-                                  Nk = Nk, kappa.init = kappa, mcmc = target, brn = brnin, seed = 1234)
-      g_list = result$g_list
-      y.plot = glist_to_plotdf_2D(g_list, gridmat, truefun = f0, alpha1 = 0.9, alpha2 = 0.95)
-      MSE_list[m, a] = mean((y.plot$truefun - y.plot$mean)^2)
-      print(m)
-   }
-}
-
-## Plot 4. For the replicated result ##
-library(tidyverse)
-MSE.df = data.frame(MSE_list)
-colnames(MSE.df) = nlist
-MSE.df.plot = gather(MSE.df, key = "n", value = "MSE")
-MSE.df.plot$n <- factor(MSE.df.plot$n, levels = nlist)
-ggplot(MSE.df.plot) + 
-   geom_boxplot(aes(x = n, y = MSE))
-## save as Rdata
-save(MSE.df.plot, file = "Result_Manuscript/MSE_dataframe/MSE_SPDE_2D.RData")
+a = 1
+n = nlist[a]
+filename = paste0("Result_Manuscript/obs_n2D", n, ".RData")
+load(filename)
+m = 1
+X = df[((m-1)*n+1):(m*n), c(1, 2)]
+Z = df$Z[((m-1)*n+1):(m*n)]
+result = sample.exact2D.seq(X, Z, sigsq = 0.1^2, # N.pr = function(x){return(1)},
+                            N.pr = dpoi5,
+                            Nk = Nk, kappa.init = kappa, mcmc = target, brn = brnin, seed = 1234)
+g_list = result$g_list
+y.plot = glist_to_plotdf_2D(g_list, gridmat, truefun = f0, alpha1 = 0.9, alpha2 = 0.95)
 
 ################## plot ###################
 grandmin <- round(min(y.plot$truefun) - 0.5, 2)
@@ -91,15 +75,13 @@ plot_upp2 <- ggplot(y.plot, aes(x1, x2)) +
 
 final_plot = ggarrange(plotlist = list(plot_true, plot_mean, 
                                        plot_low2, plot_upp2), nrow = 2, ncol = 2)
-final_plot
 
-## difference plot
 
-plot_diff <- ggplot(y.plot, aes(x1, x2)) +
-   geom_contour_filled(aes(z = truefun - mean),  
-                       show.legend = TRUE) + 
-   labs(title = paste0("Difference plot, MSE = ", round(MSE, 4))) +themegg
-plot_diff
+fileloc = "Result_Manuscript/heatmap/"
+pdf(file = paste0(fileloc, "heatmap_SPDE_2D.pdf"))
+print(final_plot)
+dev.off()
+
 
 N_list = tail(result$N_list, target)
 par(mfrow = c(1, 2))
