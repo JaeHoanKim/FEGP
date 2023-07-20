803a296e758851e5df171c9e74c9c010423a779d
Jaehoan Kim <k1mjh6561@tamu.edu>
2023-07-20 20:40:13 +0900

    add codes to generate plot for burnin change

--- MSE_comparison/MSE_comparison_for_integrated.R
+++ MSE_comparison/MSE_comparison_for_integrated.R
@@ -2,7 +2,7 @@ rm(list = ls())
 library(ggplot2)
 library(tidyverse)
 source("Result_Manuscript/GraphAesthetics.R")
-filename = "MSE_list_generated_data"
+filename = "MSE_comparison/comparison_burnin/brn100/MSE_list_generated_data"
 load(paste0(filename, "_1D.RData"))
 MSE_list_1D = data.frame(MSE_list_1D)
 M = 50
@@ -37,3 +37,36 @@ MSE_plot_2D <- ggplot(MSE_list_2D) +
 MSE_plot_1D
 MSE_plot_2D
 
+
+## Save plots ##
+library(gridExtra)
+# MSE.plot = grid.arrange(MSE.plot.1D, MSE.plot.2D, ncol = 2)
+# ggsave("Graphs/MSE_plot.pdf", MSE.plot)
+
+
+pdf(file = "Graphs/MSE_plot_flipped.pdf", width = 12, height = 4)
+grid.arrange(MSE_plot_1D, MSE_plot_2D, ncol = 2)
+dev.off()
+
+### MSE as the number of burnin differs
+filename = "MSE_comparison/comparison_burnin/brn1000/MSE_list_generated_data"
+
+load(paste0(filename, "_2D.RData"))
+MSE_list_2D = data.frame(MSE_list_2D)
+
+M = 50
+n_list = c(200, 500, 1000)
+MSE_list_2D$method = c(rep("GPI", M), rep("SPDE", M), rep("NNGP", M))
+colnames(MSE_list_2D) = c(n_list, "method")
+MSE_list_2D <- MSE_list_2D %>%
+   gather(key = "n", value = "MSE", `200`:`1000`) %>%
+   mutate(n = factor(n, levels = c("200", "500", "1000")))
+
+MSE_plot_2D_1000 <- ggplot(MSE_list_2D) +
+   geom_boxplot(aes(x = factor(n), y = MSE, color = method)) + labs(title = "MSE comparison plot - 2D", x = "n") +
+   theme1
+
+
+pdf(file = "Graphs/MSE_plot_burnin.pdf", width = 12, height = 4)
+grid.arrange(MSE_plot_2D, MSE_plot_2D_1000, ncol = 2)
+dev.off()
