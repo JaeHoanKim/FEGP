8bc4a419f62477f74ced256336133ca9bb42746b
Jaehoan Kim <k1mjh6561@tamu.edu>
2023-07-25 00:46:31 +0900

    Split MSE comparison plot for 1D and 2D

--- MSE_comparison/MSE_integrated_2D.R
+++ MSE_comparison/MSE_integrated_2D.R
@@ -16,7 +16,18 @@ sourceCpp("1D/GPI/inv_chol.cpp")
 
 ### 1. true function setting & data generation
 
-f0_1D = function(x){return (x^2 + sin(x))}
+alpha = 0.5
+
+# f0_1D = function(x){return (x^2 + sin(x))}s
+f0_1D = function(x, trun = 200){
+   value = 0
+   for(j in 1:trun){
+      value = value + cos(pi * (j - 1/2) * x) * sin(j) * j^(- alpha - 1)
+   }
+   return(value * sqrt(2))
+}
+
+
 f0_2D = function(x, y){return(x^2 + sqrt(abs(y-0.5)) + sin(8*x))}
 # next try if it still preserves the pattern: abs(y-0.5)
 
@@ -53,70 +64,6 @@ for(i in 1:length(nlist)){
    Z = f0_2D(X[, 1], X[, 2]) + rnorm(n*M) * 0.1
    df_2D[[i]] = data.frame(X, Z)
 }
-
-### 2. MSE calculation - 1D
-
-source("1D/GPI/functions_GPI.R")
-source("1D/GPI/functions_GPI_sampling.R")
-MSE_list_GPI1D = matrix(nrow = M, ncol = length(nlist))
-
-### Parallel computing ###
-nworkers <- detectCores() # Initialize the cluster
-cl <- makeCluster(nworkers)
-registerDoParallel(cl)
-##########################
-for(a in 1:length(nlist)){
-   n = nlist[a] # the number of observed data; 200, 500, 1000
-   df = df_1D[[a]]
-   output <- foreach (m = 1:M, .packages = c("Matrix", "fields", "FastGP")) %dopar% {  
-      # m th dataset among M = 50 dataset
-      X = df$X[((m-1)*n+1):(m*n)]
-      Y = df$Z[((m-1)*n+1):(m*n)]
-      result = sample.ESS.seq(X, Y, sigsq = 0.1^2, Nk = Nk, nu.in = nu, l.in = 1/kappa,
-                              N.pr = const,
-                              mcmc = target, brn=0, brn.ESS = brn.ESS)
-      g.plot = tail(result$g_list, target) # choosing last `target` samples
-      obs = data.frame(X, Y)
-      y.plot = glist_to_plotdf(g.plot, grid.plot, truefun = f0_1D, alpha1 = 0.95, alpha2 = 0.9)
-      print(m)
-      mean((y.plot$true - y.plot$mean)^2)
-   }
-   MSE_list_GPI1D[, a] = simplify(output)
-}
-stopCluster(cl)
-
-
-source("1D/SPDE/functions_SPDE_sampling.R")
-source("1D/SPDE/functions_SPDE.R")
-MSE_list_SPDE1D = matrix(nrow = M, ncol = length(nlist))
-grid.plot = c(0:1000)/1000
-
-### Parallel computing ###
-nworkers <- detectCores() # Initialize the cluster
-cl <- makeCluster(nworkers)
-registerDoParallel(cl)
-
-for(a in 1:length(nlist)){
-   n = nlist[a]
-   df = df_1D[[a]]
-   output <- foreach (m = 1:M, .packages = c("Matrix", "fields", "FastGP")) %dopar% {
-      X = df$X[((m-1)*n+1):(m*n)]
-      Y = df$Z[((m-1)*n+1):(m*n)]
-      result = sample.exact.seq(X, Y, sigsq = 0.1^2, Nk = Nk, N.pr = const, beta = 2,
-                                kappa.init = kappa, mcmc = target, brn=0, seed = 1234)
-      g.plot = tail(result$g_list, target) # choosing last `target` samples
-      obs = data.frame(X, Y)
-      y.plot = glist_to_plotdf(g.plot, grid.plot, truefun = f0_1D, alpha1 = 0.95, alpha2 = 0.9)
-      mean((y.plot$true - y.plot$mean)^2)
-   }
-   MSE_list_SPDE1D[, a] = simplify(output)
-}
-
-stopCluster(cl)
-
-MSE_list_1D = rbind(MSE_list_GPI1D, MSE_list_SPDE1D)
-save(MSE_list_1D, file = "MSE_list_generated_data_1D.RData")
-
 
 ### 3. MSE calculation - 2D
 
