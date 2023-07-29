caaf6452e6577275415a96f705de32676ac01797
Jaehoan Kim <k1mjh6561@tamu.edu>
2023-07-29 21:38:09 +0900

    remove duplication of kappa.pr

--- 1D/SPDE/functions_SPDE_sampling.R
+++ 1D/SPDE/functions_SPDE_sampling.R
@@ -1,6 +1,6 @@
 ## Sample exact using the original GP
 
-sample.exact.seq = function(X, Y, kappa.pr = function(x){return(1)}, Nk, N.pr, kappak, kappa.pr, tausqk, tausq.pr,
+sample.exact.seq = function(X, Y, Nk, N.pr, kappak, kappa.pr, tausqk, tausq.pr,
                         beta, mcmc, brn, sigsq = 0.01, kappa.init, seed = 1000){
    ## X, Y: given data
    ## N.pr: prior distribution of N (function)
@@ -11,6 +11,7 @@ sample.exact.seq = function(X, Y, kappa.pr = function(x){return(1)}, Nk, N.pr, k
    em = mcmc + brn # total number of sampling
    g_list = list()
    log_prob_N_list = vector(length = length(Nk * kappak * tausqk))
+   N_list = kappa_list = tausq_list = vector(length = em)
    var_grid = list()
    mean_grid = list()
    prec_grid = list()
@@ -39,21 +40,21 @@ sample.exact.seq = function(X, Y, kappa.pr = function(x){return(1)}, Nk, N.pr, k
    }
    # sample from p(N|D)
    set.seed(seed)
-   N_list = sample(Nk, size = em, replace = TRUE, prob = exp(log_prob_N_list - log_prob_N_list[length(Nk)]))
-   
-   for(k in 1:length(Nk)){
-      N = Nk[k]
-      index = which(N_list == N)
-      if (length(index) >= 1){
-         set.seed(seed * k)
-         g_samples <- mvtnorm::rmvnorm(n = length(index), mean = mean_grid[[k]], sigma = var_grid[[k]],
-                                      checkSymmetry = FALSE)
+   param_index_list = sample(1:(N1 * N2 * N3), size = em, replace = TRUE, prob = exp(log_prob_N_list - log_prob_N_list[N1 * N2 * N3]))
+   for(param_index in 1:(N1 * N2 * N3)){
+      index = which(param_index_list == param_index)
+      if(length(index >= 1)){
+         set.seed(seed * param_index)
+         g_samples <- mvtnorm::rmvnorm(n = length(index), mean = mean_grid[[param_index]], sigma = var_grid[[param_index]],
+                                       checkSymmetry = FALSE)
          for(j in 1:length(index)){
             g_list[[(index[j])]] = g_samples[j, ]
+            N_list[index[j]] = Nk[param_index %/% (N2 * N3) + 1]
+            kappa_list[index[j]] = kappak[(param_index %% (N2 * N3)) %/% N3 + 1]
+            tausq_list[index[j]] = tausqk[param_index %% N3]
          }
       }
    }
-   return(list(g_list = g_list, N_list = N_list, log_prob_N_list = log_prob_N_list))
+   return(list(g_list = g_list, N_list = N_list, kappa_list = kappa_list,
+               tausq_list = tausq_list, log_prob_N_list = log_prob_N_list))
 }
-
-
