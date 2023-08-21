5f13fbe34ad2015de838ce43b010e379c09ee772
Jaehoan Kim <k1mjh6561@tamu.edu>
2023-08-21 14:19:07 -0400

    change target number to be 2

--- Result_Manuscript/Time_comparison_2D_whole.R
+++ Result_Manuscript/Time_comparison_2D_whole.R
@@ -25,7 +25,7 @@ for(a in 1:length(nlist)){
 }
 
 ## setting for the sampling
-target = 500; brn = 0
+target = 2; brn = 0
 brnin = 0
 Nk = 10 # fix Nk to be a singleton vector
 const = function(x){return(1)}
