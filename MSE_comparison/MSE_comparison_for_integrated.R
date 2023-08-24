18a06d94ace2e921f26895e95674af3fb4b84de8
Jaehoan Kim <jaehoan.kim@duke.edu>
2023-08-24 13:20:47 -0400

    revise code for 1D and 2D plot lists

--- MSE_comparison/MSE_comparison_for_integrated.R
+++ MSE_comparison/MSE_comparison_for_integrated.R
@@ -3,41 +3,37 @@ library(ggplot2)
 library(tidyverse)
 source("Result_Manuscript/GraphAesthetics.R")
 filename = "MSE_comparison/comparison_trueftn/MSE_list_generated_data"
-load(paste0(filename, "_1D_1.RData"))
-MSE_list_1D = data.frame(MSE_list_1D)
-M = 50
-n_list = c(200, 500, 1000)
-MSE_list_1D$method = c(rep("GPI", M), rep("SPDE", M))
-colnames(MSE_list_1D) = c(n_list, "method")
-MSE_list_1D <- MSE_list_1D %>%
-   gather(key = "n", value = "MSE", `200`:`1000`) %>%
-   mutate(n = factor(n, levels = c("200", "500", "1000")))
+MSE_plot_1D = list()
+MSE_plot_2D = list()
+for(index in c(1:2)){
+   ## save plots for 1D
+   load(paste0(filename, "_1D_", index, ".RData"))
+   MSE_list_1D = data.frame(MSE_list_1D)
+   M = 50
+   n_list = c(200, 500, 1000)
+   MSE_list_1D$method = c(rep("GPI", M), rep("SPDE", M))
+   colnames(MSE_list_1D) = c(n_list, "method")
+   MSE_list_1D <- MSE_list_1D %>%
+      gather(key = "n", value = "MSE", `200`:`1000`) %>%
+      mutate(n = factor(n, levels = c("200", "500", "1000")))
+   MSE_plot_1D[[index]] <- ggplot(MSE_list_1D) +
+      geom_boxplot(aes(x = factor(n), y = MSE, color = method)) + labs(title = "MSE comparison plot - 1D", x = "n") +
+      theme1 + ylim(c(0, max(MSE_list_1D$MSE)))
    
-MSE_plot_1D <- ggplot(MSE_list_1D) +
-   geom_boxplot(aes(x = factor(n), y = MSE, color = method)) + labs(title = "MSE comparison plot - 1D", x = "n") +
-   theme1 + ylim(c(0, max(MSE_list_1D$MSE)))
-
-MSE_plot_1D
-
-### plot for 2D
-
-load(paste0(filename, "_2D_1.RData"))
-MSE_list_2D = data.frame(MSE_list_2D)
-
-M = 50
-n_list = c(200, 500, 1000)
-MSE_list_2D$method = c(rep("GPI", M), rep("SPDE", M), rep("NNGP", M))
-colnames(MSE_list_2D) = c(n_list, "method")
-MSE_list_2D <- MSE_list_2D %>%
-   gather(key = "n", value = "MSE", `200`:`1000`) %>%
-   mutate(n = factor(n, levels = c("200", "500", "1000")))
-
-MSE_plot_2D <- ggplot(MSE_list_2D) +
-   geom_boxplot(aes(x = factor(n), y = MSE, color = method)) + labs(title = "MSE comparison plot - 2D", x = "n") +
-   theme1
-
-MSE_plot_1D
-MSE_plot_2D
+   ## save plots for 2D
+   load(paste0(filename, "_2D_", index, ".RData"))
+   MSE_list_2D = data.frame(MSE_list_2D)
+   M = 50
+   n_list = c(200, 500, 1000)
+   MSE_list_2D$method = c(rep("GPI", M), rep("SPDE", M), rep("NNGP", M))
+   colnames(MSE_list_2D) = c(n_list, "method")
+   MSE_list_2D <- MSE_list_2D %>%
+      gather(key = "n", value = "MSE", `200`:`1000`) %>%
+      mutate(n = factor(n, levels = c("200", "500", "1000")))
+   MSE_plot_2D[[index]] <- ggplot(MSE_list_2D) +
+      geom_boxplot(aes(x = factor(n), y = MSE, color = method)) + labs(title = "MSE comparison plot - 2D", x = "n") +
+      theme1
+}
 
 
 ## Save plots ##
@@ -46,8 +42,8 @@ library(gridExtra)
 # ggsave("Graphs/MSE_plot.pdf", MSE.plot)
 
 
-pdf(file = "Graphs/MSE_plot_flipped.pdf", width = 12, height = 4)
-grid.arrange(MSE_plot_1D, MSE_plot_2D, ncol = 2)
+pdf(file = "Graphs/MSE_plot_1D.pdf", width = 12, height = 4)
+grid.arrange(MSE_plot_1D[[1]], MSE_plot_1D[[2]], ncol = 2)
 dev.off()
 
 ### MSE as the number of burnin differs
