085bc4430b5c8980f3504358734a520b4edc8999
Jaehoan Kim <jaehoan.kim@duke.edu>
2023-08-30 12:34:44 -0400

    Add plots

--- Marginal_likelihood_1D.R
+++ Marginal_likelihood_1D.R
@@ -12,23 +12,19 @@ library(foreach)
 library(rSPDE)
 library(doParallel)
 library(tidyverse)
+source("Result_Manuscript/GraphAesthetics.R")
 sourceCpp("1D/GPI/inv_chol.cpp")
 
 ### 1. true function setting & data generation
 
 alpha = 1.4
-
-# f0_1D = function(x, trun = 500){
-#    value = 0
-#    for(j in 1:trun){
-#       value = value + sin(j) * cos(pi * (j - 1/2) * x) * j^(- alpha - 1)
-#    }
-#    return(value * sqrt(2))
-# }
-f0_1D = function(x){return(x^2 + sin(x))}
-
-const = function(x){return(1)}
-
+f0_1D = function(x, trun = 500){
+   value = 0
+   for(j in 1:trun){
+      value = value + sin(j) * cos(pi * (j - 1/2) * x) * j^(- alpha - 1)
+   }
+   return(value * sqrt(2))
+}
 
 M = 1
 nlist = c(200, 500, 1000)
@@ -51,7 +47,7 @@ brn = 0
 brn.ESS = 1000
 # setting for the Matern parameters
 kappak = seq(2, 5, 0.3)
-tausqk = seq(1, 5, 0.5)
+tausqk = 1
 Nk = c(6, 8, 10, 14, 18, 22, 26, 30)
 kappa.pr = tausq.pr = N.pr = const 
 beta = 4
@@ -60,7 +56,6 @@ const = function(x){
    return(1)
 }
 
-grid.plot = c(0:1000)/1000
 
 ### 2. MSE calculation - 1D
 
@@ -68,6 +63,8 @@ source("1D/GPI/functions_GPI.R")
 source("1D/GPI/functions_GPI_sampling.R")
 m = 1
 log_prob_list_GPI = list()
+prob_N_GPI = list()
+prob_kappa_GPI = list()
 ##########################
 for(a in 1:length(nlist)){
    n = nlist[a] # the number of observed data; 200, 500, 1000
@@ -80,12 +77,18 @@ for(a in 1:length(nlist)){
                            tausqk = tausqk, tausq.pr = tausq.pr, 
                            beta = beta, mcmc = target, brn=0, brn.ESS = brn.ESS)
    log_prob_list_GPI[[a]] = result$log_prob_N_list
+   log_prob_mat = matrix(log_prob_list_GPI[[a]], nrow = length(kappak))
+   log_prob_mat = log_prob_mat - max(log_prob_mat)
+   prob_mat = exp(log_prob_mat)
+   prob_N_GPI[[a]] = colSums(prob_mat) / sum(prob_mat)
+   prob_kappa_GPI[[a]] = colSums(t(prob_mat)) / sum(prob_mat)
 }
 
 source("1D/SPDE/functions_SPDE_sampling.R")
 source("1D/SPDE/functions_SPDE.R")
 log_prob_list_SPDE = list()
-grid.plot = c(0:1000)/1000
+prob_N_SPDE = list()
+prob_kappa_SPDE = list()
 m = 1
 for(a in 1:length(nlist)){
    n = nlist[a]
@@ -97,11 +100,21 @@ for(a in 1:length(nlist)){
                              tausqk = tausqk, tausq.pr = tausq.pr,
                              mcmc = target, brn=0, seed = 1234)
    log_prob_list_SPDE[[a]] = result$log_prob_N_list
+   # calculate the marginal likelihood of N
+   log_prob_mat = matrix(log_prob_list_SPDE[[a]], nrow = length(kappak))
+   log_prob_mat = log_prob_mat - max(log_prob_mat)
+   prob_mat = exp(log_prob_mat)
+   prob_N_SPDE[[a]] = colSums(prob_mat) / sum(prob_mat)
+   prob_kappa_SPDE[[a]] = colSums(t(prob_mat)) / sum(prob_mat)
 }
 
-## calculate the marginal likelihood of N
+prob_N_SPDE <- data.frame(prob_N = unlist(prob_N_SPDE), n = rep(nlist, each = length(Nk)), 
+                        N = rep(Nk, times = length(nlist))) %>%
+   mutate(n = factor(n))
+ggplot(prob_N_SPDE, aes(x = N, y = prob_N)) + geom_point() + geom_line(aes(color = n)) + theme1
+   
+prob_kappa_SPDE <- data.frame(prob_kappa = unlist(prob_kappa_SPDE), n = rep(nlist, each = length(kappak)), 
+                          kappa = rep(kappak, times = length(nlist))) %>%
+   mutate(n = factor(n))
+ggplot(prob_kappa_SPDE, aes(x = kappa, y = prob_kappa)) + geom_point() + geom_line(aes(color = n)) + theme1
 
-stopCluster(cl)
-
-MSE_list_1D = rbind(MSE_list_GPI1D, MSE_list_SPDE1D)
-save(MSE_list_1D, file = "MSE_list_generated_data_1D_2.RData")
