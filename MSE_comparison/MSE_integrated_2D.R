0c96248547d1e855ea416b597130acae59efa775
Jaehoan Kim <jaehoan.kim@duke.edu>
2023-08-30 18:46:40 -0400

    Change the support according to the marginal likelihood result

--- MSE_comparison/MSE_integrated_2D.R
+++ MSE_comparison/MSE_integrated_2D.R
@@ -43,16 +43,16 @@ nu = beta - d/2
 const = function(x){
    return(1)
 }
-Nk = c(4, 6, 8, 10, 12)
+Nk = c(6, 8, 10, 14, 18, 22, 26, 30)
 N.pr = kappa.pr = tausq.pr = const
 tausq.pr = function(x){return(invgamma::dinvgamma(x, 1, 1))}
 kappa.pr = function(x){return(1/x^2)}
-kappak = seq(2, 5, 1)
-tausqk = seq(2, 5, 1)
+kappak = seq(1, 6, 0.5)
+tausqk = 1
 
 target = 2500
 brn = 0
-brn.ESS = 1000
+brn.ESS = 100
 
 ### 3. MSE calculation - 2D
 
