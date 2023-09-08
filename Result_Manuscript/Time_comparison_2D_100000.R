112e0490e80ed344b731ae0355364f42bd3b2916
Jaehoan Kim <jaehoan.kim@duke.edu>
2023-09-08 19:54:52 -0400

    change time unit into seconds

--- Result_Manuscript/Time_comparison_2D_100000.R
+++ Result_Manuscript/Time_comparison_2D_100000.R
@@ -106,29 +106,34 @@ target = 2
 fileloc = "Result_Manuscript/Time_dataframe/time_2D_large_"
 load(paste0(fileloc, target, ".RData"))
 time_comparison_large_n = time_comparison_unify
+## unit by second
+time_comparison_large_n$time = time_comparison_large_n$time / 1e9
 fileloc = "Result_Manuscript/Time_dataframe/time_2D_"
 load(paste0(fileloc, target, ".RData"))
+time_comparison_unify$time = time_comparison_unify$time / 1e9
 time_comparison_unify = rbind(time_comparison_large_n, time_comparison_unify)
 time.plot.2 <- ggplot(time_comparison_unify) +
-   geom_boxplot(aes(x = factor(n), y = log_time, color = method)) +
-   labs(title = paste0(target, " samples"), x = "n", y = "log(time)") + theme1
+   geom_boxplot(aes(x = factor(n), y = time, color = method)) +
+   labs(title = paste0(target, " samples"), x = "n", y = "time(s)") + theme1
 
 target = 500
 fileloc = "Result_Manuscript/Time_dataframe/time_2D_large_"
 load(paste0(fileloc, target, ".RData"))
 time_comparison_large_n = time_comparison_unify
+time_comparison_large_n$time = time_comparison_large_n$time / 1e9
 fileloc = "Result_Manuscript/Time_dataframe/time_2D_"
 load(paste0(fileloc, target, ".RData"))
+time_comparison_unify$time = time_comparison_unify$time / 1e9
 time_comparison_unify = rbind(time_comparison_large_n, time_comparison_unify)
 time.plot.500 <- ggplot(time_comparison_unify) +
-   geom_boxplot(aes(x = factor(n), y = log_time, color = method)) +
-   labs(title = paste0(target, " samples"), x = "n", y = "log(time)") + theme1
+   geom_boxplot(aes(x = factor(n), y = time, color = method)) +
+   labs(title = paste0(target, " samples"), x = "n", y = "time(s)") + theme1
 
 
 library(gridExtra)
-pdf(file = "Graphs/Time_whole_plot_updated.pdf", width = 12, height = 4)
+# pdf(file = "Graphs/Time_whole_plot_updated.pdf", width = 12, height = 4)
 grid.arrange(time.plot.2, time.plot.500, ncol = 2)
-dev.off()
+# dev.off()
 
 # 
 # time.plot.1 <- ggplot(time_comparison_large_n) +
